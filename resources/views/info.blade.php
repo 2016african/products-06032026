8465524142866d66175b3ec99268e9ccf0af0f92
Pauls <pauls@bila.com>
2026-03-20 13:05:45 +0200

    Izveidots /info ar paragrāfa elementu lorem25

---
+++ resources/views/info.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus eaque impedit quasi deserunt animi sapiente repellat non blanditiis itaque. Expedita excepturi illum quos reprehenderit laborum?</p>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/about', function () {
     return '<h1> About this page </h1>';
 });
 
+Route::get('/info', function () {
+    return view('info');
+});
+
